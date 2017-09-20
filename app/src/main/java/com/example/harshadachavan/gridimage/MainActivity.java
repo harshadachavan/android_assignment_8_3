2cb5d68499af55edcc66310fc907b9d1038f30b3
harshada <harsh.chvn@gmail.com>
2017-09-20 18:34:04 +0530

    Grid Imageview

---
+++ app/src/main/java/com/example/harshadachavan/gridimage/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.harshadachavan.gridimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    GridView grid;
+
+    int[] imageId = {
+            R.drawable.gingerbread,
+            R.drawable.honeycomb,
+            R.drawable.icecream,
+            R.drawable.jelly,
+            R.drawable.kitkat,
+            R.drawable.lollipop
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CustomGrid adapter = new CustomGrid(MainActivity.this, imageId);
+        grid=(GridView) findViewById(R.id.grid);
+        grid.setAdapter(adapter);
+        /*grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "You Clicked at " +imageId[+ position], Toast.LENGTH_SHORT).show();
+
+            }
+        }); */
+
+    }
+}
